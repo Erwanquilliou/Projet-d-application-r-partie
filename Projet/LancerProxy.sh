45cb1b3ebf7f4a79d533421a868728d92f53ed1b
Erwanquilliou <163106067+Erwanquilliou@users.noreply.github.com>
2025-06-13 11:05:48 +0200

    modification script

--- Projet/LancerProxy.sh
+++ Projet/LancerProxy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd "$(dirname "$0")/src" || exit 1
-echo "lancer ServiceWaze"
-javac -cp ".;../json-20131018.jar" API/ProxyServer.java
-java  -cp ".;../json-20131018.jar" API.ProxyServer localhost localhost
+echo "lancer Proxy"
+javac -cp ".;../json-20131018.jar" API/ProxyServerHTTPS.java
+java  -cp ".;../json-20131018.jar" API.ProxyServerHTTPS $1 $2
 cd - > /dev/null
