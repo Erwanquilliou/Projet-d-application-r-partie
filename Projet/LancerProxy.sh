f0871b94f26c653b344782e32a3f33ce89439eb5
Nilecram <marcelin.ray@protonmail.com>
2025-06-16 13:11:09 +0200

    Modification des scripts pour linux
    ne pas oublier de les chmod +x !!!

--- Projet/LancerProxy.sh
+++ Projet/LancerProxy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd "$(dirname "$0")/src" || exit 1
+echo "compiler Proxy"
+javac -cp ".:../json-20131018.jar" API/ProxyServerHTTPS.java
 echo "lancer Proxy"
-javac -cp ".;../json-20131018.jar" API/ProxyServerHTTPS.java
-java  -cp ".;../json-20131018.jar" API.ProxyServerHTTPS $1 $2
+java  -cp ".:../json-20131018.jar" API.ProxyServerHTTPS $1 $2
 cd - > /dev/null
